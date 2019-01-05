685d33b97a52d308235d7fa87445ad4a2d6536c4
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-01-05 20:36:13 +0100

    Configure publishing on Github Pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-import Image from '../components/image'
 import SEO from '../components/seo'
 
 const IndexPage = () => (
@@ -12,7 +11,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      Hello world
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
