0114b25b8b3286192a906ba44e2c73bafd94a6be
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-01-19 10:31:12 +0100

    Load all used fonts from Google Fonts

--- src/config/typography.js
+++ src/config/typography.js
@@ -3,9 +3,24 @@ import Typography from "typography";
 const typography = new Typography({
   baseFontSize: "18px",
   baseLineHeight: 1.45,
-  headerFontFamily: ["Lato"],
+  headerFontFamily: ["Lato", "San Francisco", "Helvetica", "sans-serif"],
   headerWeight: 400,
-  bodyFontFamily: ["Source Sans Pro", "serif"],
+  bodyFontFamily: [
+    "Source Sans Pro",
+    "San Francisco",
+    "Helvetica",
+    "sans-serif",
+  ],
+  googleFonts: [
+    {
+      name: "Lato",
+      styles: ["400"],
+    },
+    {
+      name: "Source Sans Pro",
+      styles: ["400", "700"],
+    },
+  ],
 });
 
 export default typography;
