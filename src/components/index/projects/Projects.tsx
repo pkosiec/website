1652522aa6a78c42e3eb30df88c4132c116ac805
Paweł Kosiec <pawel.kosiec@gmail.com>
2019-10-11 23:01:26 +0200

    Prepare basic template for Projects grid

--- src/components/index/projects/Projects.tsx
+++ src/components/index/projects/Projects.tsx
@@ -1,4 +1,6 @@
 import * as React from "react";
+import styled from "@config/styled";
+import { media } from "@config/responsive";
 import { Section } from "@components/layout/shared/Section";
 import { Project } from "@components/index/projects/Project";
 
@@ -6,19 +8,42 @@ interface ProjectsProps {
   projects: Project[];
 }
 
+const List = styled.ul`
+  margin: 40px -20px 0 -20px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: stretch;
+`;
+
+const Item = styled.li`
+  margin: 0 20px 40px 20px;
+  flex-basis: calc(50% - 40px);
+  list-style-type: none;
+  display: block;
+
+  *:last-child {
+    margin-bottom: auto;
+  }
+
+  ${media.phone`
+    flex-basis: 100%;
+    margin: 0 0 20px;
+  `}
+`;
+
 const Projects: React.FunctionComponent<ProjectsProps> = ({
   projects = [],
 }) => {
   return (
     <Section>
-      <h1>Featured projects</h1>
-      <ul>
+      <h1>Projects & Initiatives</h1>
+      <List>
         {projects.map(project => (
-          <li key={project.title}>
+          <Item key={project.title}>
             <Project project={project} />
-          </li>
+          </Item>
         ))}
-      </ul>
+      </List>
     </Section>
   );
 };
